b5c2c5f71308e851df4599fe2cc6b0c81eadb731
BruceFortes <brucefortes@hotmail.com>
2025-09-16 19:23:40 +0000

    Nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,47 +1,68 @@
 #include <stdio.h>
 
+void moverBispo(int passoExterno, int maxExterno, int passoInterno, int maxInterno)
+{
+    if (passoExterno > maxExterno)
+        return;
+    for (int j = passoInterno; j <= maxInterno; j++)
+    {
+        printf("Passo %d-%d: Diagonal Superior Direita -> Cima e Direita\n", passoExterno, j);
+    }
+    moverBispo(passoExterno + 1, maxExterno, passoInterno, maxInterno);
+}
+
+void moverTorre(int passo, int max)
+{
+    if (passo > max)
+        return;
+    printf("Passo %d: Direita\n", passo);
+    moverTorre(passo + 1, max);
+}
+
+void moverRainha(int passo, int max)
+{
+    if (passo > max)
+        return;
+    printf("Passo %d: Esquerda\n", passo);
+    moverRainha(passo + 1, max);
+}
+
 int main()
 {
+
     const int BISPO_MOV = 5;
     const int TORRE_MOV = 5;
     const int RAINHA_MOV = 8;
-    const int CAVALO_MOV_1 = 2;
-    const int CAVALO_MOV_2 = 1;
 
     printf("Movimento do Bispo:\n");
-    for (int i = 1; i <= BISPO_MOV; i++)
-    {
-        printf("Passo %d: Diagonal Superior Direita -> ", i);
-        printf("Cima e Direita\n");
-    }
+    moverBispo(1, BISPO_MOV, 1, 1);
     printf("\n");
 
     printf("Movimento da Torre:\n");
-    int passoTorre = 1;
-    while (passoTorre <= TORRE_MOV)
-    {
-        printf("Passo %d: Direita\n", passoTorre);
-        passoTorre++;
-    }
+    moverTorre(1, TORRE_MOV);
     printf("\n");
 
-    int passoRainha = 1;
     printf("Movimento da Rainha:\n");
-    do
-    {
-        printf("Passo %d: Esquerda\n", passoRainha);
-        passoRainha++;
-    } while (passoRainha <= RAINHA_MOV);
+    moverRainha(1, RAINHA_MOV);
     printf("\n");
 
     printf("Movimento do Cavalo:\n");
-    for (int i = 1; i <= CAVALO_MOV_1; i++)
-    { 
-        int j = 1;
-        while (j <= CAVALO_MOV_2)
-        { 
-            printf("Passo %d-%d: Baixo Baixo e Esquerda\n", i, j);
-            j++;
+    int casasVertical = 2;
+    int casasHorizontal = 1;
+    for (int i = 1; i <= casasVertical + 1; i++)
+    {
+        for (int j = 1; j <= casasHorizontal + 1; j++)
+        {
+            if (i == 2 && j == 2)
+            {
+
+                continue;
+            }
+            if (i > casasVertical || j > casasHorizontal)
+            {
+                break;
+            }
+            printf("Passo Cavalo: %d para cima, %d para direita\n", i, j);
         }
     }
     printf("\n");
